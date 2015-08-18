1e31dd6549d9415d2e566a8eec01212c11e5ba82
Jesse McGraw <jlmcgraw@gmail.com>
2015-08-17 20:49:30 -0400

    Comments, move stuff around, fix some warnings

--- ping_hosts_in_acl.pl
+++ ping_hosts_in_acl.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/perl
 # Copyright (C) 2015  Jesse McGraw (jlmcgraw@gmail.com)
 #
-# Ping hosts (and smaller subnets) mentioned in ACL to see what's still viable
+# Ping hosts mentioned in config to see what's still viable, annotate with DNS name and status
+# Enumerate count of hosts in subnets, normalize wildcard and CIDR entries to subnet form
+# ( eg 10.0.0.1/255.255.255.0) to prevent confusion (but note this makes shown config invalid)
 #
-# Snip out your ACL from your config and save it to a text file, supply this
-# text file as a parameter to this utility
+# Point this utility to your router/switch/firewall config, or snip out an ACl of interest
+# to a separate text file
 #
 #-------------------------------------------------------------------------------
 # This program is free software: you can redistribute it and/or modify
@@ -22,7 +24,7 @@
 #-------------------------------------------------------------------------------
 
 #TODO
-# Provide color-coded HTML output
+# Normalize how subnets are shown in config
 # Process all hosts in subnets.  Should probably limit the size of subnets processed
 # Quit processing a subnet on the first active response since that indicates
 #   the subnet is still valid
@@ -33,6 +35,7 @@
 #BUGS
 #   (FIXED) Freaks out on normal host/subnet masks
 #DONE
+#   Provide color-coded HTML output
 #   Option for type of ping test
 #   Option for number of threads
 
@@ -48,15 +51,12 @@ use Storable;
 # use File::Basename;
 use threads;
 use Thread::Queue;
-
-# Use Share
 use threads::shared;
 
 # use Benchmark qw(:hireswallclock);
 use Getopt::Std;
 use FindBin '$Bin';
 use vars qw/ %opt /;
-use Net::Ping;
 
 # The sort routine for Data::Dumper
 $Data::Dumper::Sortkeys = sub {
@@ -81,18 +81,17 @@ use lib "$FindBin::Bin/lib";
 
 #Additional modules
 use Modern::Perl '2014';
-
-# use Regexp::Common;
 use Params::Validate qw(:all);
 use NetAddr::IP;
+use Net::Ping;
 
 #Smart_Comments=0 perl my_script.pl
 # to run without smart comments, and
 #Smart_Comments=1 perl my_script.pl
-use Smart::Comments;
+use Smart::Comments -ENV;
 
 #Use this to not print warnings
-no if $] >= 5.018, warnings => "experimental";
+#no if $] >= 5.018, warnings => "experimental";
 
 #Define the valid command line options
 my $opt_string = 'p:t:';
@@ -113,15 +112,17 @@ unless ( getopts( "$opt_string", \%opt ) ) {
 #Default method of testing remote connectivity
 my $ping_method = 'tcp';
 
+#Set ping method depending on OS
 if ( $Config{archname} =~ m/win/ix ) {
-    print "$Config{osname}\n";
-    print "$Config{archname}\n";
+
+    #     print "$Config{osname}\n";
+    #     print "$Config{archname}\n";
 
     #You can ping without root on windows
     $ping_method = 'icmp';
 }
 
-#What method does the user want to ping via
+#What method does the user want to ping via?
 if ( $opt{p} ) {
 
     #If something  provided on the command line use it instead
@@ -136,19 +137,22 @@ say "Testing connectivity by method $ping_method";
 #The maximum number of simultaneous threads
 my $max_threads = 32;
 
+#Does the user want to use more or less threads?
 if ( $opt{t} ) {
     $max_threads = $opt{t};
     say "Using $max_threads threads";
 }
 
-#Where known networks are stored
+#Where known networks are stored (created by bgp_asn_path_via_snmp.pl)
 my $known_networks_filename = 'known_networks.stored';
 
-# my %known_networks;
+#a hash reference for those networks
 my $known_networks_ref;
 
+#A number from 0 - 255
 my $ipOctetRegex = qr/(?: 25[0-5] | 2[0-4]\d | [01]?\d\d? )/x;
 
+#an IP address is 4 octets separated by .
 my $ipv4AddressRegex = qr/$ipOctetRegex\.
                           $ipOctetRegex\.
 			  $ipOctetRegex\.
@@ -158,15 +162,11 @@ my $ipv4AddressRegex = qr/$ipOctetRegex\.
 if ( -e $Bin . "/$known_networks_filename" ) {
     say "Loading $known_networks_filename ...";
 
-    # #This is a Data::Dumper output from "bgp_asn_path_via_snmp.pl"
-    # #with the default route manually removed
-    # %known_networks = do $Bin . "/$known_networks_filename";
-
     #Read in hash from 'storable' format
     $known_networks_ref = retrieve($known_networks_filename);
 }
 
-# print Dumper $known_networks_ref;
+## known_networks_ref: $known_networks_ref
 
 #Call main routine
 exit main(@ARGV);
@@ -179,7 +179,7 @@ sub main {
         #Note which file we're working on
         say $filename;
 
-        #Open the input and output files
+        #Open the input file
         open my $filehandle, '<', $filename or die $!;
 
         #Read in the whole file
@@ -216,25 +216,25 @@ sub main {
 
         }
 
-        #Smart_Comments
-        # %found_networks_and_hosts
+        ##Smart_Comments found_networks_and_hosts: %found_networks_and_hosts
         #print Dumper \%found_networks_and_hosts;
 
         #Make one long string out of the array
+        #This is probably unecessary but I haven't bothered to change code yet
         my $scalar_of_lines = join "\n", @array_of_lines;
 
-        #Gather info about each found host and put back into ACL
+        #Gather info about each found host and annotate original text with that info
         parallel_process_hosts( \%found_networks_and_hosts,
             \$scalar_of_lines );
 
-        #Gather info about each found network and put back into ACL
+        #Gather info about each found network and annotate original text with that info
         parallel_process_networks( \%found_networks_and_hosts,
             \$scalar_of_lines );
 
         #Print out the annotated ACL
         # say $scalar_of_lines;
 
-        #For future HTML output
+        #Very basic HTML output
         open my $filehandleHtml, '>', $filename . '-tested.html' or die $!;
 
         #Print a simple html beginning to output
@@ -345,17 +345,17 @@ sub find_hosts_and_nets_in_line {
     #Combine the two network arrays into one
     push( @net_matches, @net_cidr_matches, @net_mask_matches );
 
-    #Return two arrays
+    #Return two array references
     return ( \@host_matches, \@net_matches );
 }
 
 sub add_found_hosts_to_shared_hash {
+
+    #Put the hosts we found into a hash
     my ( $hosts_in_line_ref, $found_networks_and_hosts_ref )
         = validate_pos( @_, { type => ARRAYREF }, { type => HASHREF }, );
 
     foreach my $host_ip ( @{$hosts_in_line_ref} ) {
-
-        #         say "\t\tHOST: $host_ip";
 
         #Make sure this new key is shared
         if ( !exists $found_networks_and_hosts_ref->{hosts}{$host_ip} ) {
@@ -374,12 +374,16 @@ sub add_found_hosts_to_shared_hash {
         else {
             say
                 "$host_ip already exists!-------------------------------------------------------------------";
+            $found_networks_and_hosts_ref->{'hosts'}{$host_ip}{'count'}
+                += 1;
         }
 
     }
 }
 
 sub add_found_networks_to_shared_hash {
+
+    #Put the subnets we found into a hash
     my ( $nets_in_line_ref, $found_networks_and_hosts_ref )
         = validate_pos( @_, { type => ARRAYREF }, { type => HASHREF }, );
 
@@ -389,20 +393,22 @@ sub add_found_networks_to_shared_hash {
 
         my ( $address, $mask ) = split( '/', $network_and_mask );
 
-        #         say $network_and_mask;
-        #         say "\t\tNET: $address mask $mask";
-
         my ( $possible_matches_ref, @one, $number_of_hosts );
 
-        #What to do if this looks like a subnet mask
+        #Does this look like a subnet mask?
         if ( is_subnet_mask($mask) ) {
 
+            #Try to make a NetAddr object from it
             my $acl_subnet = NetAddr::IP->new("$address/$mask");
 
             #If we could create the subnet...
             if ($acl_subnet) {
+
+                #This is the number of hosts in that subnet
                 $number_of_hosts = $acl_subnet->num();
 
+                #These are other bits of information about this subnet we may
+                # want to save at some point
                 # say "acl_subnet: $network / $mask_norm_dotted";
                 # $ip_addr = $acl_subnet->addr;
                 # $network_mask = $acl_subnet->mask;
@@ -415,14 +421,13 @@ sub add_found_networks_to_shared_hash {
 
         }
         else {
-
+            #We're guessing this is a wildcard mask
             #Get a list of all addresses that match this host/wildcard_mask combination
             $possible_matches_ref = list_of_matches_acl( $address, $mask );
 
             #Save that array reference to another array
             @one = @{$possible_matches_ref};
 
-            # print @one;
             #Get a count of how many elements in that array
             $number_of_hosts = @{$possible_matches_ref};
 
@@ -434,34 +439,39 @@ sub add_found_networks_to_shared_hash {
 
         #Make sure this new key is shared
         if ( !exists $found_networks_and_hosts_ref->{'networks'}
-            { $address . ' ' . $mask } )
+            {$network_and_mask} )
         {
             #Share the key, which deletes existing data
-            $found_networks_and_hosts_ref->{'networks'}
-                { $address . ' ' . $mask } = &share( {} );
+            $found_networks_and_hosts_ref->{'networks'}{$network_and_mask}
+                = &share( {} );
 
             #How many hosts in the possible matches list
             $found_networks_and_hosts_ref->{'networks'}
-                { $address . ' ' . $mask }{'number_of_hosts'}
-                = "$number_of_hosts";
+                {$network_and_mask}{'number_of_hosts'} = "$number_of_hosts";
 
             #Default having no specific route for this network
             $found_networks_and_hosts_ref->{'networks'}
-                { $address . ' ' . $mask }{'status'} = "via DEFAULT";
+                {$network_and_mask}{'status'} = "via DEFAULT";
 
             #                 #All possible matches to the mask
             #                 $found_networks_and_hosts_ref->{'networks'}
-            #                     { $address . ' ' . $mask }{'list_of_hosts'} = "@one";
+            #                     { $network_and_mask }{'list_of_hosts'} = "@one";
         }
         else {
             say
                 "$address $mask already exists!-------------------------------------------------------------------";
+            $found_networks_and_hosts_ref->{'networks'}
+                {$network_and_mask}{'count'} += 1;
         }
 
     }
 }
 
 sub parallel_process_hosts {
+
+    #Ping and DNS lookup all found hosts in parallel fashion
+    #Then add this info back into source config text
+
     my ( $found_networks_and_hosts_ref, $scalar_of_lines_ref )
         = validate_pos( @_, { type => HASHREF }, { type => SCALARREF }, );
 
@@ -472,7 +482,7 @@ sub parallel_process_hosts {
     my $q = Thread::Queue->new();
 
     # Queue up all of the hosts for the threads
-    $q->enqueue($_) for keys $found_networks_and_hosts_ref->{'hosts'};
+    $q->enqueue($_) for keys %{ $found_networks_and_hosts_ref->{'hosts'} };
 
     #Nothing else to queue
     $q->end();
@@ -496,10 +506,12 @@ sub parallel_process_hosts {
     $_->join() for @thr;
 
     #Smart comment for debug
-    # %found_networks_and_hosts;
+    ## found_networks_and_hosts: %found_networks_and_hosts;
 
-    #Substitute info we found back into the lines of the ACL
-    foreach my $host_key ( keys $found_networks_and_hosts_ref->{'hosts'} ) {
+    #Substitute info we found back into the lines of the config
+    foreach
+        my $host_key ( keys %{ $found_networks_and_hosts_ref->{'hosts'} } )
+    {
 
         #Get the info we found for this host
         my $host_name
@@ -507,8 +519,10 @@ sub parallel_process_hosts {
         my $host_status
             = $found_networks_and_hosts_ref->{'hosts'}{$host_key}{'status'};
 
+        #What color to use depends on status of host
         my $text_color = $host_status eq 'UP' ? 'lime' : 'red';
 
+        #What to add
         my $text_to_insert = "$host_name : $host_status";
 
         #If there's no DNS entry for this host don't include IP in substitution
@@ -516,24 +530,25 @@ sub parallel_process_hosts {
             $text_to_insert = "$host_status";
         }
 
-        #Substitute it back into the ACL
+        #Substitute it back into the config
         ${$scalar_of_lines_ref}
             =~ s/([^\d] \s+ )$host_key (\s* [^\dm])/$1<font color = "$text_color">$host_key<\/font> [$text_to_insert]$2/ixg;
     }
 }
 
 sub parallel_process_networks {
-    my ( $found_networks_and_hosts_ref, $scalar_of_lines_ref )
-        = validate_pos( @_, { type => HASHREF }, { type => SCALARREF }, );
 
     #Now prepare to process info network
     #in a parallel fashion so it doesn't take forever
+    #Add found information back into original configuration text
+    my ( $found_networks_and_hosts_ref, $scalar_of_lines_ref )
+        = validate_pos( @_, { type => HASHREF }, { type => SCALARREF }, );
 
     # A new empty queue
     my $q = Thread::Queue->new();
 
     # Queue up all of the networks for the threads
-    $q->enqueue($_) for keys $found_networks_and_hosts_ref->{'networks'};
+    $q->enqueue($_) for keys %{ $found_networks_and_hosts_ref->{'networks'} };
 
     #No more data to queue
     $q->end();
@@ -560,8 +575,8 @@ sub parallel_process_networks {
     # ## %found_networks_and_hosts;
 
     #Substitute info we found back into the lines of the ACL
-    foreach
-        my $network_key ( keys $found_networks_and_hosts_ref->{'networks'} )
+    foreach my $network_key (
+        keys %{ $found_networks_and_hosts_ref->{'networks'} } )
     {
 
         #Get the info we found for this host
@@ -573,9 +588,11 @@ sub parallel_process_networks {
             = $found_networks_and_hosts_ref->{'networks'}{$network_key}
             {'status'};
 
+        #Color of the text depends on whether we found a specific route for this network (green)
+        # or just a default (red)
         my $text_color = $status =~ /default/ix ? 'red' : 'lime';
 
-        #Substitute it back into the ACL
+        #Substitute it back into the original configuration text
         ${$scalar_of_lines_ref}
             =~ s/$network_key/<font color = "$text_color">$network_key<\/font> [ $number_of_hosts hosts <font color = "$text_color">$status<\/font> ]/g;
     }
@@ -583,6 +600,8 @@ sub parallel_process_networks {
 }
 
 sub process_hosts_thread {
+
+    #This is the body of the threads that run in parallel
     my ( $host_ip, $found_networks_and_hosts_ref )
         = validate_pos( @_, { type => SCALAR }, { type => HASHREF }, );
 
@@ -596,13 +615,15 @@ sub process_hosts_thread {
 
     # say "Thread $id: $host_ip -> $name";
 
+    # I'm not sure if we should lock or not...
     # lock($found_networks_and_hosts_ref)->{'hosts'};
     $found_networks_and_hosts_ref->{'hosts'}{$host_ip}{'dns_name'}
         = "$name";
 
+    #How long to wait for a response
     my $timeout = 3;
 
-    #This needs to be 'tcp' or 'udp' on unix systems (or run as root)
+    #This needs to be 'tcp' or 'udp' on unix systems (or run script as root)
     my $p = Net::Ping->new( $ping_method, $timeout )
         or die "Thread $id: Unable to create Net::Ping object ";
 
@@ -624,6 +645,8 @@ sub process_hosts_thread {
 }
 
 sub process_networks_thread {
+
+    #This is the body of the threads that run in parallel
     my ( $network_and_mask, $found_networks_and_hosts_ref )
         = validate_pos( @_, { type => SCALAR }, { type => HASHREF }, );
 
@@ -635,17 +658,20 @@ sub process_networks_thread {
     # say "Thread $id: $network_and_mask";
 
     #Split into components
-    my ( $network, $network_mask ) = split( ' ', $network_and_mask );
+    my ( $network, $network_mask ) = split( '/', $network_and_mask );
 
     my $acl_subnet;
 
     #Does the mask appear to be a subnet mask?
     if ( is_subnet_mask($network_mask) ) {
+
+        #Create a NetAddr object
         $acl_subnet = NetAddr::IP->new("$network/$network_mask");
     }
     else {
         #Assume it's a wildcard_mask
         #This little bit of magic inverts the wildcard mask to a netmask.  Copied from somewhere on the net
+        #Note that it doesn't work with non contigous masks
         my $mask_wild_dotted = $network_mask;
         my $mask_wild_packed = pack 'C4', split /\./, $mask_wild_dotted;
 
@@ -668,9 +694,11 @@ sub process_networks_thread {
         # $range           = $subnet->range();
 
         #Test it against all known networks...
+        #TODO Maybe we should sort by prefix length to get most specific match
         foreach my $known_network ( keys %{$known_networks_ref} ) {
             ## $known_network
             # say "known_network: $known_network";
+
             #Everything will match default route, let's skip it
             next if ( $known_network eq '0.0.0.0/0' );
 
@@ -717,6 +745,8 @@ sub usage {
 }
 
 sub pretty_addr {
+
+    #Return DNS entry for an IP or just the IP if no DNS
     my ($addr) = validate_pos( @_, { type => SCALAR }, );
 
     my ( $hostname, $aliases, $addrtype, $length, @addrs )
@@ -832,7 +862,7 @@ sub test_octet {
         }
     }
 
-    #Return array of which values from 0..255 match
+    #Return array of which values from 0..255 matched
     return \@matches;
 }
 
@@ -876,6 +906,8 @@ sub is_subnet_mask {
 }
 
 sub dump_to_file {
+
+    #Simplify dumping a hash to a file
     my ( $file, $aoh_ref ) = @_;
 
     open my $fh, '>', $file
